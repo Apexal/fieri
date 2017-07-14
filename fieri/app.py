8b8c34365f16a84866811d379b085bfdcc737b9c
Frank Matranga <thefrankmatranga@gmail.com>
2017-07-14 00:03:40 -0400

    Ready for login

--- fieri/app.py
+++ fieri/app.py
@@ -1,17 +1,23 @@
 import os
 from flask import Flask, request, session, redirect, url_for, render_template, flash
+import flask_login
 
 app = Flask(__name__)
 app.secret_key = os.environ['SECRET_KEY']
 
 @app.route('/')
 def index():
-    return render_template('index.html', username=session['username'])
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        session['username'] = request.form['username']
+        #session['username'] = request.form['username']
         return redirect(url_for('index'))
 
-    return render_template('login.html', username=session['username'])
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    
+    return 'Logged out'
